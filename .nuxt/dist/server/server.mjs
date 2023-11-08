86e06467fe93835c65146e46641eab8219e1433b
NationofFoxes <stuart.william.auld@gmail.com>
2023-11-08 16:21:17 +0900

    static site

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/stuar/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ _nuxt/home.27238d17.js
@@ -0,0 +1,1 @@
+import{_ as t,c as o,v as s,a,o as c}from"./entry.ba38a5d1.js";const n={},_={class:"py-20 px-10 text-center"},r=a("div",{class:"mt-5 mx-auto text-center opacity-25 text-sm"}," [Home Layout] ",-1);function l(e,m){return c(),o("main",_,[s(e.$slots,"default"),r])}const i=t(n,[["render",l]]);export{i as default};
